d19f9edef743188370ed0b69f525b19990c9e08d
ryandhikabaa <dhika2018rplrus2@gmail.com>
2020-12-01 20:06:45 +0700

    Add Badge Notif

--- app/src/main/java/com/tangria/spa/bookingku/Activity/Main/MainActivity.java
+++ app/src/main/java/com/tangria/spa/bookingku/Activity/Main/MainActivity.java
@@ -14,7 +14,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tangria.spa.bookingku.Activity.Notification.NotificationActivity;
 import com.tangria.spa.bookingku.Fragment.Base.BaseFragment;
@@ -29,10 +31,12 @@ public class MainActivity extends AppCompatActivity implements MainView, BottomN
     final MainPresenter presenter = new MainPresenter();
     @BindView(R.id.bottomnav)
     BottomNavigationView bottomNavigationView;
-    @BindView(R.id.toolbar)
+    @BindView(R.id.toolbarNotif)
     Toolbar toolbar;
     @BindView(R.id.tv_title)
     TextView tvTitle;
+    @BindView(R.id.ivToNotification)
+    ImageView ivToNotification;
 
     //
     @Override
@@ -63,6 +67,15 @@ public class MainActivity extends AppCompatActivity implements MainView, BottomN
         presenter.showHomeFragmentForFirstTime();
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         Log.e(TAG, "onCreate: " + getSharedPreferences("firebase_token", MODE_PRIVATE).getString("firebase_token", ""));
+
+        ivToNotification = findViewById(R.id.ivToNotification);
+        ivToNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     //=========== lifecycle ===========//
@@ -107,21 +120,21 @@ public class MainActivity extends AppCompatActivity implements MainView, BottomN
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_toolbar, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_toolbar, menu);
+//        return true;
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_notification) {
-            Intent intent = new Intent(this, NotificationActivity.class);
-            startActivity(intent);
-        }
-        return true;
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == R.id.action_notification) {
+//            Intent intent = new Intent(this, NotificationActivity.class);
+//            startActivity(intent);
+//        }
+//        return true;
+//    }
 
     @Override
     public void goToFragment(BaseFragment currentFragment, BaseFragment fragment) {
